acf160b2f33477770e0fe93246b650ba654e47c3
anant mehra <anant0007mehra@gmail.com>
2022-02-06 20:57:44 +0530

    test

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
-    // But it's just a dummy/ demo anyways
+    // But it's just a dummy/ demo anyway
     setIsLoggedIn(true);
   };
 
